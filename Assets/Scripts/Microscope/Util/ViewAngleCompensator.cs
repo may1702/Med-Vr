fbaf44b470ef406fd11cfe70878d9ac9b7f23702
David Robideau <robideau@iastate.edu>
2016-10-15 16:51:19 -0500

    Rebuilt Assets folder

---
+++ Assets/Scripts/Microscope/Util/ViewAngleCompensator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+/// <summary>
+/// Simple property of scope cameras - should the viewing angle affect the image seen through the lens?
+/// For a more "realistic" experience, yes. For usability purposes, maybe not.
+/// </summary>
+public class ViewAngleCompensator : MonoBehaviour {
+
+    public bool MatchViewAngle = false;
+    public GameObject EyeCamRig;
+
+    void Update() {
+        if (MatchViewAngle) AdjustScopeCamAngle();
+    }
+
+    /// <summary>
+    /// Adjust the angles of the scope camera to match those of the HMD camera rig
+    /// </summary>
+    private void AdjustScopeCamAngle() {
+        GameObject scopeCam = GameObject.FindWithTag("ScopeCam");
+        try {
+            scopeCam.transform.eulerAngles = EyeCamRig.transform.eulerAngles;
+        } catch (NullReferenceException) {
+            Debug.Log("Either the scope camera or the eye camera rig could not be found.");
+        }
+    }
+	
+}
