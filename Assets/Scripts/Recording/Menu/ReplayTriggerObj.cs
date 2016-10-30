da51ee712c8330f444a9d017678b4d99614ee2b7
robideau <robideau@iastate.edu>
2016-10-30 13:42:10 -0500

    Revert "Revert "Replays now work in VR, hands automatically tracked""
    This reverts commit 5fa42cb5b9b2576dd5662fac5d6c54439606cef3.

--- Assets/Scripts/Recording/Menu/ReplayTriggerObj.cs
+++ Assets/Scripts/Recording/Menu/ReplayTriggerObj.cs
@@ -16,7 +16,13 @@ public class ReplayTriggerObj : MonoBehaviour {
 
     public void OnReplaySceneLoaded(Scene scene, LoadSceneMode mode) {
         if (scene.name == "RecordingDebug") {
+            
+            SteamVR_ControllerManager manager = GameObject.FindWithTag("VRCamRig").GetComponent<SteamVR_ControllerManager>();
+            manager.left.GetComponent<SteamVR_TrackedObject>().enabled = false;
+            manager.right.GetComponent<SteamVR_TrackedObject>().enabled = false;
+            
             GameObject.Find("STATIC").GetComponent<Reenactor>().PrepForReplay(FullPath);
+
             Destroy(gameObject);
         }
     }
