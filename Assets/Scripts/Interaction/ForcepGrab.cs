72014e4233da57f8f456431187457a0559910059
Med-Vr <may17002@gmail.com>
2017-02-18 16:03:31 -0600

    Added forceps interaction

---
+++ Assets/Scripts/Interaction/ForcepGrab.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForcepGrab : MonoBehaviour {
+
+    public List<GameObject> GrabbableObjects;
+    private ForcepsAnimControl fAnim;
+    private Transform _grabbedParentRef;
+    private GameObject _grabbedObj;
+    private Vector3 _grabbedInitialScale;
+
+    private void Awake()
+    {
+        fAnim = GetComponent<ForcepsAnimControl>();
+        fAnim.vrtk.TriggerReleased += new VRTK.ControllerInteractionEventHandler(DropObject);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (fAnim.anim.GetBool("close") && GrabbableObjects.Contains(collision.gameObject))
+        {
+            Debug.Log("grabbing");
+            GrabObject(collision);
+        }
+    }
+
+    private void GrabObject(Collision collision)
+    {
+        GrabbableObjects.Remove(collision.gameObject);
+        _grabbedObj = collision.gameObject;
+        _grabbedParentRef = collision.gameObject.transform.parent;
+        _grabbedInitialScale = collision.gameObject.transform.localScale;
+        collision.gameObject.transform.SetParent(transform, true);
+        collision.gameObject.transform.localScale = _grabbedInitialScale / transform.localScale.x; 
+    }
+
+    public void DropObject(object sender, VRTK.ControllerInteractionEventArgs e)
+    {
+        _grabbedObj.transform.SetParent(_grabbedParentRef);
+        GrabbableObjects.Add(_grabbedObj);
+        _grabbedObj.transform.localScale = _grabbedInitialScale;
+
+    }
+
+}
---
+++ Assets/Scripts/Interaction/ForcepsAnimControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForcepsAnimControl : MonoBehaviour {
+
+    public VRTK.VRTK_ControllerEvents vrtk;
+    public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        vrtk.TriggerPressed += new VRTK.ControllerInteractionEventHandler(CloseForceps);
+        vrtk.TriggerReleased += new VRTK.ControllerInteractionEventHandler(IdleForceps);
+        anim = GameObject.Find("forceps-rigged").GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void CloseForceps(object sender, VRTK.ControllerInteractionEventArgs e)
+    {
+        bool closeBool = anim.GetBool("close");
+        Debug.Log("Close: " + closeBool.ToString());
+        anim.SetBool("close", true);
+    }
+
+    public void IdleForceps(object sender, VRTK.ControllerInteractionEventArgs e)
+    {
+        anim.SetBool("close", false);
+    }
+}
