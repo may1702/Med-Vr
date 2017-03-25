0410c55c3916daf50b2662a3d6bda13c2339663a
Med-Vr <may17002@gmail.com>
2017-03-25 15:47:02 -0500

    Suturing works with cloth

--- Assets/Scripts/Interaction/SuturePoints.cs
+++ Assets/Scripts/Interaction/SuturePoints.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Obi;
 using UnityEngine;
+using System;
 
 public class SuturePoints : MonoBehaviour {
 
@@ -18,6 +19,20 @@ public class SuturePoints : MonoBehaviour {
     private GameObject needleObj;
     private Vector3[] _verts;
 
+    private SutureData currentSutureData;
+
+    public struct SutureData {
+        public int indexA;
+        public int indexB;
+        public int particleIndexA;
+        public int particleIndexB;
+        public Vector3 initialA;
+        public Vector3 initialB;
+        public Vector3 newA;
+        public Vector3 newB;
+        public Vector3 initialNeedlePos;
+    }
+
     private void Awake()
     {
         vrtk.TriggerPressed += new VRTK.ControllerInteractionEventHandler(EndSuture);
@@ -26,22 +41,34 @@ public class SuturePoints : MonoBehaviour {
 
     private void Update()
     {
-        //if (_sutureActive) UpdateCloth();
+        //if (_sutureActive) UpdateCloth(GetComponent<MeshFilter>().sharedMesh);
     }
 
 
 	public void SuturePointsTrigger(int indA, int indB, GameObject needleObj)
     {
-        Cloth.Solver.enabled = false;
+        currentSutureData = new SutureData();
+
+        //Cloth.Solver.enabled = false;
         int indexA = _targetMesh.triangles[indA];
         int indexB = _targetMesh.triangles[indB];
+        currentSutureData.indexA = indexA;
+        currentSutureData.indexB = indexB;
+
         _sutureActive = true;
         pointA = new Vector3(_targetMesh.vertices[indexA].x, _targetMesh.vertices[indexA].y, _targetMesh.vertices[indexA].z);
         pointB = new Vector3(_targetMesh.vertices[indexB].x, _targetMesh.vertices[indexB].y, _targetMesh.vertices[indexB].z);
+        currentSutureData.initialA = transform.TransformPoint(pointA);
+        currentSutureData.initialB = transform.TransformPoint(pointB);
+
         this.needleObj = needleObj;
         _sutureMidpoint = new Vector3((pointA.x + pointB.x) / 2.0f, (pointA.y + pointB.y) / 2.0f, (pointA.z + pointB.z) / 2.0f);
         _verts = _targetMesh.vertices;
         GetComponent<SuturePointRecorder>().RecordPoints = false;
+        currentSutureData.initialNeedlePos = needleObj.transform.position;
+
+        GetRelatedIndices();
+
         StartCoroutine(SuturePointsCo(indexA, indexB));
     }
 
@@ -49,8 +76,11 @@ public class SuturePoints : MonoBehaviour {
     {
         while(_sutureActive)
         {
-            //lerpTimescale = (needleObj.GetComponent<Rigidbody>().velocity.magnitude / 10) * Time.deltaTime;
-            lerpTimescale += .15f * Time.deltaTime;
+            //Debug.Log(Vector3.Distance(needleObj.transform.position, transform.TransformPoint(currentSutureData.initialB)));
+            //lerpTimescale = Vector3.Distance(needleObj.transform.position, transform.TransformPoint(currentSutureData.initialB));
+            //lerpTimescale += (needleObj.GetComponent<Rigidbody>().velocity.magnitude / 10) * Time.deltaTime;
+            //lerpTimescale += .15f * Time.deltaTime;
+            lerpTimescale = (Vector3.Distance(currentSutureData.initialNeedlePos, needleObj.transform.position)) / Vector3.Magnitude(currentSutureData.initialA - currentSutureData.initialB);
             Vector3 newA = new Vector3(
                 Mathf.Lerp(pointA.x, _sutureMidpoint.x, lerpTimescale),
                 Mathf.Lerp(pointA.y, _sutureMidpoint.y, lerpTimescale),
@@ -65,6 +95,8 @@ public class SuturePoints : MonoBehaviour {
             _verts[indexB] = newB;
             pointA = newA;
             pointB = newB;
+            currentSutureData.newA = newA;
+            currentSutureData.newB = newB;
 
             //Update mesh
             GetComponent<MeshFilter>().sharedMesh.vertices = _verts;
@@ -73,7 +105,9 @@ public class SuturePoints : MonoBehaviour {
             GetComponent<MeshFilter>().mesh.RecalculateBounds();
             GetComponent<MeshCollider>().sharedMesh.vertices = _verts;
             GetComponent<MeshCollider>().sharedMesh.RecalculateBounds();
-          
+
+            UpdateCloth(GetComponent<MeshCollider>().sharedMesh, currentSutureData);
+
             yield return new WaitForEndOfFrame();
         }
         yield return null;
@@ -81,37 +115,55 @@ public class SuturePoints : MonoBehaviour {
 
     public void EndSuture(object sender, VRTK.ControllerInteractionEventArgs e)
     {
-        GetComponent<SuturePointRecorder>().RecordPoints = true;
-        _sutureActive = false;
-        Mesh newMesh = Instantiate(GetComponent<MeshCollider>().sharedMesh);
-        Cloth.Solver.enabled = true;
-        UpdateCloth(newMesh);
+        if (_sutureActive)
+        {
+            GetComponent<SuturePointRecorder>().RecordPoints = true;
+            _sutureActive = false;
+            Mesh newMesh = Instantiate(GetComponent<MeshCollider>().sharedMesh);
+            //Cloth.Solver.enabled = true;
+            //Update cloth
+            UpdateCloth(GetComponent<MeshCollider>().sharedMesh, currentSutureData);
+            currentSutureData = new SutureData();
+        }
+     
     }
 
-    public void UpdateCloth(Mesh newMesh)
+    public void UpdateCloth(Mesh newMesh, SutureData sData)
+    {   
+        Cloth.positions[sData.particleIndexA] = sData.newA;
+        Cloth.positions[sData.particleIndexB] = sData.newB;
+        Cloth.PushDataToSolver(new ObiSolverData(ObiSolverData.ParticleData.ALL));
+        
+    }
+
+    private void GetRelatedIndices()
     {
-        //ObiMeshTopology updatedTopology = ScriptableObject.CreateInstance(typeof(ObiMeshTopology)) as ObiMeshTopology;
-        //updatedTopology.InputMesh = newMesh;
-        //updatedTopology.Generate();
-        //Cloth.sharedTopology = updatedTopology;
-        //Cloth.topology = updatedTopology;
-        Cloth.clothMesh = newMesh;
-        Cloth.clothMesh.vertices = newMesh.vertices;
-        Cloth.clothMesh.RecalculateBounds();
-        Cloth.clothMesh.RecalculateNormals();
-        Cloth.sharedMesh = newMesh;
+        int particleIndexA = 0;
+        int particleIndexB = 0;
+        for (int i = 0; i < Cloth.positions.Length; i++)
+        {
+            Vector3 pPos = Cloth.GetParticlePosition(i);
+            //Debug.Log("Position " + i + " = " + pPos);
+            if (VectorSimilar(pPos, currentSutureData.initialA, 0.1f)) particleIndexA = i;
+            if (VectorSimilar(pPos, currentSutureData.initialB, 0.1f)) particleIndexB = i;
+        }
+        currentSutureData.particleIndexA = particleIndexA;
+        currentSutureData.particleIndexB = particleIndexB;
 
-        Cloth.CommitResultsToMesh();
-        Cloth.GetMeshDataArrays(newMesh);
-        Cloth.Solver.FreeParticles(Cloth.particleIndices);
-        Cloth.Solver.AllocateParticles(5000);
-        Cloth.Solver.UpdateActiveParticles();
+        Debug.Log("Initial A: " + currentSutureData.initialA);
+        Debug.Log("Initial B: " + currentSutureData.initialB);
+        Debug.Log("A, B: (" + currentSutureData.particleIndexA + ", " + currentSutureData.particleIndexB + ")");
+    }
 
-        //Cloth.topology.GenerateVisualVertexBuffer();
-        //Cloth.sharedTopology.GenerateVisualVertexBuffer();
+    private bool VectorSimilar(Vector3 A, Vector3 B, float tolerance)
+    {
+        if (Math.Abs(A.x - B.x) < tolerance &&
+            Math.Abs(A.y - B.y) < tolerance &&
+            Math.Abs(A.z - B.z) < tolerance)
+        {
+            return true;
+        }
 
-        //Cloth.sharedTopology.Generate();
-        //Cloth.topology.Generate();
-        //newCloth.sharedTopology = updatedTopology;
+        return false;
     }
 }
