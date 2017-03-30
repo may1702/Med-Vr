72bac0549698403471dbc27b6bd800c5adb1901c
Med-Vr <may17002@gmail.com>
2017-03-29 22:23:42 -0500

    Populated surgery scene and added videos playback. (having difficulties because the video file is too large for github)

---
+++ Assets/Videos/play.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class play : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        ((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
